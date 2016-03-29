633ecd998fe5cb6247bfc01c810b8fa6e3b32a96
Bhagwat <rohit.bhagwat@accenture.com>
2016-03-29 14:56:13 +0530

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name devOpsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the devOpsApp
+ */
+angular.module('devOpsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name devOpsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the devOpsApp
+ */
+angular.module('devOpsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
